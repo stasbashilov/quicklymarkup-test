01c661e111678315679b89b50aebbe299e82ed23
Stas Bashilov <stasbashilov@gmail.com>
2019-08-12 23:17:03 +0300

    added project

---
+++ js/modules/base.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('[name="project-status"]').select2({
+        minimumResultsForSearch: Infinity
+    });
+
+    $('[name="user-role"]').select2({
+        minimumResultsForSearch: Infinity
+    });
+});
